3f253664cb834e2121dedf92aea9b759629ad1c4
JaiRaga <raga.j.santhosh@gmail.com>
2020-02-25 23:32:24 +0530

    Updates

---
+++ precache-manifest.23250a7b4645d5098fd288a3dff30757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669ba7ce5d86c5953df3f793c6451cbd",
+    "url": "/FreeCodeCamp-Frontend-Certification-Projects/tree/master/random-quote-machine/index.html"
+  },
+  {
+    "revision": "944d781dc53d1c2984c7",
+    "url": "/FreeCodeCamp-Frontend-Certification-Projects/tree/master/random-quote-machine/static/css/main.2a54dce9.chunk.css"
+  },
+  {
+    "revision": "7b29dcb54eec6571a51d",
+    "url": "/FreeCodeCamp-Frontend-Certification-Projects/tree/master/random-quote-machine/static/js/2.97603552.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/FreeCodeCamp-Frontend-Certification-Projects/tree/master/random-quote-machine/static/js/2.97603552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "944d781dc53d1c2984c7",
+    "url": "/FreeCodeCamp-Frontend-Certification-Projects/tree/master/random-quote-machine/static/js/main.7ced4be5.chunk.js"
+  },
+  {
+    "revision": "6b6f338beeb461db4704",
+    "url": "/FreeCodeCamp-Frontend-Certification-Projects/tree/master/random-quote-machine/static/js/runtime-main.55e9a66f.js"
+  }
+]);
